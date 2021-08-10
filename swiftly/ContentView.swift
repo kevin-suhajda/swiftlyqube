ba51316608917c7f76d96a7fdffbbc90206e48ee
Kevin Suhajda <kevin@nevercode.io>
2021-08-10 08:56:36 +0200

    project created

---
+++ swiftly/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  swiftly
+//
+//  Created by Kevin Suhajda on 2021. 08. 10..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ swiftly/swiftlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftlyApp.swift
+//  swiftly
+//
+//  Created by Kevin Suhajda on 2021. 08. 10..
+//
+
+import SwiftUI
+
+@main
+struct swiftlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
